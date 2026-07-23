cf1338d1c991e9908dd84a6181bf04a1a91a310c
Hussain-Saabri <dadapir19ce30@gmail.com>
2026-07-23 08:50:43 +0530

    fix: production api proxy and missing await

--- src/services/organizationService.js
+++ src/services/organizationService.js
@@ -3,8 +3,8 @@ export const organizationService = {
   async getOrganizations() {
     try{
         const response = await fetch("/api/v1/Organization");
-        const data = response.json();
-        console.log("response from organsation",data);
+        const data = await response.json();
+        console.log("response from organsation", data);
         return data;
     }catch(error){
         console.error("API Error:", error);
